5f8ea6046ac680e675a3a1f14cacded47c717275
devhorn <mdevhorn@gmail.com>
2025-03-03 16:14:39 +0100

    intergrated clouds

---
+++ models/cloud.class.js
@@ -0,0 +1,9 @@
+class Cloud extends MovableObject {
+  constructor() {
+    super().loadImage("../img/5_background/layers/4_clouds/2.png");
+    this.y = 20;
+    this.x = 0;
+    this.width = 720;
+    this.height = 200;
+  }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,7 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
+  cloud = new Cloud();
   ctx;
   canvas;
 
@@ -12,6 +13,14 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.drawImage(
+      this.cloud.img,
+      this.cloud.x,
+      this.cloud.y,
+      this.cloud.width,
+      this.cloud.height
+    );
 
     this.ctx.drawImage(
       this.character.img,
