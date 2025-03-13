3ade25b9cb6f16f3e5519ef090fa8d1274a578ce
devhorn <mdevhorn@gmail.com>
2025-03-13 07:56:26 +0100

    remove comments

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -102,7 +102,6 @@ class Endboss extends MoveableObject {
     }, 150);
     setTimeout(() => {
       clearInterval(deadInterval);
-      // GameWonOverlay anzeigen:
       document.getElementById("gameWonOverlay").classList.remove("dNone");
     }, 1000);
   }
