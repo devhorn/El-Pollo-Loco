52f2ed1ffbfb5a60eac89ab4832b76d336b751bb
devhorn <mdevhorn@gmail.com>
2025-03-17 08:01:01 +0100

    worked on endboss animation

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -4,24 +4,31 @@ class Endboss extends MoveableObject {
     "../img/4_enemie_boss_chicken/2_alert/G11.png",
     "../img/4_enemie_boss_chicken/2_alert/G12.png",
   ];
-
   imagesWalking = [
     "../img/4_enemie_boss_chicken/1_walk/G1.png",
     "../img/4_enemie_boss_chicken/1_walk/G2.png",
     "../img/4_enemie_boss_chicken/1_walk/G3.png",
     "../img/4_enemie_boss_chicken/1_walk/G4.png",
   ];
-
   imagesHurt = [
     "../img/4_enemie_boss_chicken/4_hurt/G21.png",
     "../img/4_enemie_boss_chicken/4_hurt/G22.png",
     "../img/4_enemie_boss_chicken/4_hurt/G23.png",
   ];
-
   imagesDead = [
     "../img/4_enemie_boss_chicken/5_dead/G24.png",
     "../img/4_enemie_boss_chicken/5_dead/G25.png",
     "../img/4_enemie_boss_chicken/5_dead/G26.png",
+  ];
+  imagesAttack = [
+    "../img/4_enemie_boss_chicken/3_attack/G13.png",
+    "../img/4_enemie_boss_chicken/3_attack/G14.png",
+    "../img/4_enemie_boss_chicken/3_attack/G15.png",
+    "../img/4_enemie_boss_chicken/3_attack/G16.png",
+    "../img/4_enemie_boss_chicken/3_attack/G17.png",
+    "../img/4_enemie_boss_chicken/3_attack/G18.png",
+    "../img/4_enemie_boss_chicken/3_attack/G19.png",
+    "../img/4_enemie_boss_chicken/3_attack/G20.png",
   ];
 
   x = 2600;
@@ -33,6 +40,14 @@ class Endboss extends MoveableObject {
   startingX = 2600;
   minX = 1600;
   hits = 0;
+  distanceWalked = 0;
+
+  // Neue Variablen für den Zustand und für Intervalle
+  currentState = "alert"; // mögliche Zustände: 'alert', 'moving', 'attacking', 'hurt', 'dead'
+  movementInterval = null;
+  animationInterval = null;
+  hurtInterval = null;
+  attackInterval = null;
 
   constructor() {
     super().loadImage(this.imagesAlert[0]);
@@ -40,6 +55,7 @@ class Endboss extends MoveableObject {
     this.loadImages(this.imagesWalking);
     this.loadImages(this.imagesHurt);
     this.loadImages(this.imagesDead);
+    this.loadImages(this.imagesAttack);
     this.speed = 1;
     this.endbossHitSound = new Sound("../audio/hit_endboss.wav");
     this.endbossDieSound = new Sound("../audio/die_endboss.wav", 0.2);
@@ -47,7 +63,8 @@ class Endboss extends MoveableObject {
   }
 
   animateAlert() {
-    setInterval(() => {
+    // Während der Alert-Phase
+    this.alertInterval = setInterval(() => {
       if (!this.triggerActivated) {
         this.playAnimation(this.imagesAlert);
       }
@@ -57,35 +74,61 @@ class Endboss extends MoveableObject {
   update(characterX) {
     if (!this.triggerActivated && characterX >= 2100) {
       this.triggerActivated = true;
+      clearInterval(this.alertInterval);
       this.startMovement();
     }
   }
 
   startMovement() {
+    // Verhindere mehrfaches Starten, wenn bereits "moving"
+    if (this.currentState === "moving") return;
+    this.currentState = "moving";
+    this.distanceWalked = 0;
+    // Starte Bewegungs- und Animationsintervalle
     this.animationInterval = setInterval(() => {
       this.playAnimation(this.imagesWalking);
     }, 150);
     this.movementInterval = setInterval(() => {
       if (this.x > this.minX) {
         this.moveLeft();
+        this.distanceWalked += this.speed;
+        // Nach 30 Pixeln stoppe die Bewegung und starte den Angriff
+        if (this.distanceWalked >= 30) {
+          this.clearMovementIntervals();
+          this.attack();
+        }
       } else {
-        clearInterval(this.movementInterval);
-        clearInterval(this.animationInterval);
+        this.clearMovementIntervals();
       }
     }, 1000 / 60);
   }
 
+  attack() {
+    this.currentState = "attacking";
+    this.attackInterval = setInterval(() => {
+      this.playAnimation(this.imagesAttack);
+    }, 150);
+    setTimeout(() => {
+      clearInterval(this.attackInterval);
+      // Nach dem Angriff wechsle zurück in den Bewegungszustand
+      this.startMovement();
+    }, 800);
+  }
+
   hitByBottle() {
-    if (this.isDefeated) return;
+    // Ignoriere Treffer, wenn schon in einem unpassenden Zustand
+    if (this.isDefeated || this.currentState === "hurt" || this.currentState === "dead") return;
+    this.currentState = "hurt";
     if (this.hits < 3) {
       this.endbossHitSound.play();
       this.hits++;
-      this.clearEndbossAnimations();
-      let hurtInterval = setInterval(() => {
+      this.clearAllIntervals();
+      this.hurtInterval = setInterval(() => {
         this.playAnimation(this.imagesHurt);
       }, 150);
       setTimeout(() => {
-        clearInterval(hurtInterval);
+        clearInterval(this.hurtInterval);
+        // Wechsle nur dann zurück in den Bewegungsmodus, wenn er nicht besiegt ist
         if (!this.isDefeated) {
           this.startMovement();
         }
@@ -96,9 +139,9 @@ class Endboss extends MoveableObject {
   }
 
   dieBoss() {
+    this.currentState = "dead";
     this.endbossDieSound.play();
-    this.clearEndbossAnimations();
-    this.isDefeated = true;
+    this.clearAllIntervals();
     let deadInterval = setInterval(() => {
       this.playAnimation(this.imagesDead);
     }, 150);
@@ -111,9 +154,22 @@ class Endboss extends MoveableObject {
     }, 1000);
   }
 
-  clearEndbossAnimations() {
+  clearMovementIntervals() {
     clearInterval(this.movementInterval);
     clearInterval(this.animationInterval);
+    this.movementInterval = null;
+    this.animationInterval = null;
+  }
+
+  clearAllIntervals() {
+    clearInterval(this.movementInterval);
+    clearInterval(this.animationInterval);
+    clearInterval(this.hurtInterval);
+    clearInterval(this.attackInterval);
+    this.movementInterval = null;
+    this.animationInterval = null;
+    this.hurtInterval = null;
+    this.attackInterval = null;
   }
 
   gameEndTasks() {
